08d09149b89e2ccc869aba242fe04db932b3785d
Raoul Grouls <raoul.grouls@businessdecision.nl>
2023-02-25 09:57:22 +0100

    add assignment, improve process.jl

--- pipeline/preprocess/process.jl
+++ pipeline/preprocess/process.jl
@@ -10,23 +10,23 @@ function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table! s begin
         "--datadir", "-d"
-            help = "path to input files"
-            default = "assets/raw/"
+        help = "path to input files"
+        default = "assets/raw/"
         "--processeddir", "-p"
-            help = "files after processing"
-            default = "assets/processed/"
+        help = "files after processing"
+        default = "assets/processed/"
         "--corpusfile", "-c"
-            help = "corpus.txt file with text lines"
-            default = "corpus.txt"
+        help = "corpus.txt file with text lines"
+        default = "corpus.txt"
         "--testfile", "-t"
-            help = "csv file with text lines"
-            default = "test_set.csv"
+        help = "csv file with text lines"
+        default = "test_set.csv"
     end
     return parse_args(s)
 end
 
 function corpus2mapping(datadir, processeddir, file)
-    df = DataFrame(CSV.File(datadir * file; delim="|"))
+    df = DataFrame(CSV.File(datadir * file; delim = "|"))
 
     @assert "acronym" in names(df) "The csv should contain an `acronym` columnname"
     @assert "expansion" in names(df) "The csv should contain an `expansion` columnname"
@@ -43,17 +43,19 @@ function corpus2mapping(datadir, processeddir, file)
 end
 
 
-function create_trainset(lines, mapping, processeddir)
+function create_trainset(corpuspath, mapping, processeddir)
     timestamp = Dates.format(now(), "yyyy-mm-dd-HHMMSS_")
     path = processeddir * timestamp * "train.csv"
     @info "Writing to $path"
     csvfile = open(path, "w")
     write(csvfile, "txt|label\n")
-    for line in lines
-        for (expansion, abbr) in mapping
-            if occursin(expansion, line) 
-                line = replace(line, expansion => abbr) 
-                write(csvfile, "$line|$expansion\n")
+    open(corpuspath, "r") do f
+        for line in eachline(f)
+            for (expansion, abbr) in mapping
+                if occursin(expansion, line)
+                    line = replace(line, expansion => abbr)
+                    write(csvfile, "$line|$expansion\n")
+                end
             end
         end
     end
@@ -66,14 +68,15 @@ function parse_files()
     processeddir = args["processeddir"]
     testfile = args["testfile"]
     corpusfile = args["corpusfile"]
+    @assert isfile(datadir * testfile) "The file $(datadir * testfile) does not exist"
+    @assert isfile(datadir * corpusfile) "The file $(datadir * corpusfile) does not exist"
     io = open(processeddir * "processing.log", "a+")
     logger = SimpleLogger(io)
 
     with_logger(logger) do
         @info "Reading $testfile"
         mapping = corpus2mapping(datadir, processeddir, testfile)
-        lines = readlines(datadir * corpusfile)
-        create_trainset(lines, mapping, processeddir)
+        create_trainset(datadir * corpusfile, mapping, processeddir)
     end
     flush(io)
 end
