c86dfbb9107ee65ad1bdc1dfecbc95c098ba295a
Raoul Grouls <raoul.grouls@businessdecision.nl>
2023-02-26 22:31:24 +0100

    change big_corpus.txt back to default corpus.txt

--- pipeline/preprocess/process.jl
+++ pipeline/preprocess/process.jl
@@ -17,7 +17,7 @@ function parse_commandline()
         default = "assets/processed/"
         "--corpusfile", "-c"
         help = "corpus.txt file with text lines"
-        default = "big_corpus.txt"
+        default = "corpus.txt"
         "--testfile", "-t"
         help = "csv file with text lines"
         default = "test_set.csv"
